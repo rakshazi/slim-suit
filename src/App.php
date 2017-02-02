8ff0259fac14f417ef2bce60b26633023f302a89
Nikita Chernyi <developer.nikus@gmail.com>
2017-02-02 19:34:25 +0200

    Added App with router

---
+++ src/App.php
@@ -0,0 +1,40 @@
+<?php
+namespace \Rakshazi\SlimSuit;
+
+class App extends \Slim\App
+{
+    /**
+     * Simple router
+     * $routes example:
+     * <code>
+     * [
+     *      '/some/pattern/{id}' => [
+     *          'controller' => 'controller_name',
+     *          'methods' => ['GET', 'POST'],
+     *          'action' => 'index',
+     *      ],
+     * ];
+     * </code>
+     * Default values:
+     * controller = 'default'
+     * methods = ['GET']
+     * action = 'index'
+     * @param array $routes
+     */
+    public function route(array $routes)
+    {
+        foreach ($routes as $pattern => $route) {
+            $route['controller'] = $route['controller'] ?? 'default';
+            $route['action'] = $route['action'] ?? 'index';
+            $route['methods'] = $route['methods'] ?? ['GET'];
+            $this->map($route['methods'], $pattern, function ($request, $response, $args) use ($route) {
+                return $this->getContainer()->get('controller_'.lcfirst($route['controller']))->call(
+                    $route['action'],
+                    $request,
+                    $response,
+                    $args
+                );
+            });
+        }
+    }
+}
